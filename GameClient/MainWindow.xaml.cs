2cf68a318c2b1461954df5a23f378a393a92f146
brollins90 <brollins90@gmail.com>
2014-08-13 11:41:40 -0600

    Added collision detection and keyboard movement

--- GameClient/MainWindow.xaml.cs
+++ GameClient/MainWindow.xaml.cs
@@ -29,19 +29,56 @@ namespace GameClient
             set { _Manager = value; }
         }
 
+        private Controller _CurrentController;
+
+        public Controller CurrentController
+        {
+            get { return _CurrentController; }
+            set { _CurrentController = value; }
+        }
+        
+
         public MainWindow()
         {
             InitializeComponent();
             Manager = new GameManager();
             this.DataContext = Manager;
 
-            Manager.World.Objects.Add(new Character());
+            MainGrid.Focusable = true;
+            MainGrid.Focus();
+
+            CurrentController = new Controller();
+            CurrentController.Connect(Manager);
+            CurrentController.CreateCharacter();
+
+
+            Manager.World.Objects.Add(new Character() { Position = new System.Drawing.Point(200,200) });
 
         }
 
         private void Grid_KeyDown(object sender, KeyEventArgs e)
         {
+            GameCommands keyPressed = GameCommands.Down;
+            switch (e.Key)
+            {
+                case Key.Up:
+                    keyPressed = GameCommands.Up;
+                    break;
 
+                case Key.Down:
+                    keyPressed = GameCommands.Down;
+                    break;
+
+                case Key.Left:
+                    keyPressed = GameCommands.Left;
+                    break;
+
+                case Key.Right:
+                    keyPressed = GameCommands.Right;
+                    break;
+            }
+            Console.WriteLine("KeyDown: {0}", keyPressed);
+            CurrentController.KeyDown(keyPressed);
         }
     }
 }
