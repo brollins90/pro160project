c7f348db7218932a0b886d48d2134a5d38dd2021
KeithPetrone <keithpetrone@atlanticbb.net>
2014-08-14 11:38:09 -0600

    Created Soldier Class

---
+++ GameCode/Models/Soldier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GameCode.Models
+{
+    public class Soldier : GameObject, IMovingObject, IAttackingObject
+    {
+        public Soldier(Point position) : base(position)
+        {
+
+        }
+
+        public void Attack(Point destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(Point destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
