ead8a74382ca91791569d864e6cc71a501541356
KeithPetrone <keithpetrone@atlanticbb.net>
2014-08-14 11:46:36 -0600

    Added health and modified size

--- GameCode/Models/Soldier.cs
+++ GameCode/Models/Soldier.cs
@@ -9,9 +9,17 @@ namespace GameCode.Models
 {
     public class Soldier : GameObject, IMovingObject, IAttackingObject
     {
+        private int _Health;
+        public int Health
+        {
+            get { return _Health; }
+            set { _Health = value; }
+        }
+
         public Soldier(Point position) : base(position)
         {
-
+            base.Height = 10;
+            base.Width = 10;
         }
 
         public void Attack(Point destination)
